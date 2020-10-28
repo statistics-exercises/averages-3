35a730c19d7ab5d7b9886ba1201a01fa94ff27c8
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-28 14:56:20 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def average(n) :
+  # Your code to compute the average for a set of n uniform random variables goes here.
+  
+
+# You should not need to adjust the code from here onwards
+xv, yv1, yv2 = np.linspace(1,100,100), np.zeros(100), np.zeros(100)
+for i in range(100) : 
+  yv1[i] = np.random.uniform(0,1) 
+  yv2[i] = average(100) 
+  
+plt.plot( xv, yv1, 'ro' )
+plt.plot( xv, yv2, 'ko' )
+plt.savefig("averages.png")
