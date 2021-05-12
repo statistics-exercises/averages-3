e86ce8031c3da45929705de631a542159effefd9
Gareth Tribello <gareth.tribello@gmail.com>
2021-05-12 09:47:41 +0100

    Added answers

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import numpy as np
 
 def average(n) :
   # Your code to compute the average for a set of n uniform random variables goes here.
-  
+  var = 0
+  for i in range(n) : var = var + np.random.uniform(0,1)
+  return var / n 
 
 # You should not need to adjust the code from here onwards
 xv, yv1, yv2 = np.linspace(1,100,100), np.zeros(100), np.zeros(100)
